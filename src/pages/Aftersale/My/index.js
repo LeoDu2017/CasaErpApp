bba969d01d114eee8630b7e97061b3a4d4840702
Leo2017 <2516849822@qq.com>
2019-07-26 19:38:13 +0800

    设置导航 左侧

--- src/pages/Aftersale/My/index.js
+++ src/pages/Aftersale/My/index.js
@@ -12,15 +12,17 @@ class Screen extends Component {
     static  navigationOptions =  ({navigation}) => ({
         title: '我的售后单',
         ...customNavigationOptions(navigation,
-        <View style={{flexDirection: 'row'}}>
-        <TouchableOpacity onPress={() => alert('filter')}>
-        <Image style={{height: 20, width: 20}}
-        source={require('../../../assets/images/navigation/nav_select.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => alert('search')}>
-        <Image style={{height: 20, width: 20}}
-        source={require('../../../assets/images/navigation/nav_search.png')}/>
-        </TouchableOpacity>
+        <View style={{flexDirection: 'row',right:14}}>
+            <TouchableOpacity
+                style={{marginRight:5}}
+                onPress={() => alert('filter')}>
+                <Image style={{height: 20, width: 20}}
+                    source={require('../../../assets/images/navigation/nav_select.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => alert('search')}>
+                <Image style={{height: 20, width: 20}}
+                    source={require('../../../assets/images/navigation/nav_search.png')}/>
+            </TouchableOpacity>
         </View>
         )
     });
@@ -64,6 +66,6 @@ export default connect(
         isSuccess: MAftersale.isSuccess,
     }),
     (dispatch) => ({
-        loadAction: bindActionCreators(Actions, dispatch)
+        init: bindActionCreators(Actions, dispatch)
     })
 )(Screen)
